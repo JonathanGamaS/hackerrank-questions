384ddac057fb45197d7fdfc54507d0d4472e0bd2
JonathanGamaS <jonathan_gama_@hotmail.com>
2020-09-10 11:28:12 -0300

    UPDATE: Added question triangle_quest.py in python folder

---
+++ python/triangle_quest.py
@@ -0,0 +1,9 @@
+"""
+Can you do it using only arithmetic operations, a single for loop and print statement?
+Use no more than two lines. The first line (the for statement) is already written for you. 
+You have to complete the print statement.
+"""
+
+def triangle_quest_solution():
+    for i in range(1,int(input())):  
+        print((10**(i)//9)*i)
