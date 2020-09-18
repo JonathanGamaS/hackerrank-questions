887718b831c2e5262a461dc56ffbdb9875cd887f
JonathanGamaS <jonathan_gama_@hotmail.com>
2020-09-18 18:22:11 -0300

    UPDATE: Added question python_evaluation.py in python folder

---
+++ python/python_evaluation.py
@@ -0,0 +1,6 @@
+"""
+You are given an expression in a line. Read that line as a string variable, such as var, and print the result using eval(var).
+"""
+
+def python_evaluation_sollution():
+    eval(input())
